369dac49e5d725becf7b428fa1a1bc1364826f79
Emil <hinata135@mail.ru>
2022-12-22 18:33:05 +0300

    DR

---
+++ Task_41/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Задача 41
+ * Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+ */
+
+int Input(string text)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write(text);
+            return int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Введеное не число, попробуте еще раз!");
+        }
+    }
+}
+
+while (true)
+{
+    int m = Input("Какое количество чисел хотите внести: ");
+    if (m > 0)
+    {
+        int[] numbers = new int[m];
+        int count = 0;
+        while (--m >= 0)
+        {
+            numbers[m] = Input($"Введите число {numbers.Length - m}: ");
+            if (numbers[m] > 0) count++;
+        }
+        Console.WriteLine($"Чисел больше 0: {count}");
+        break;
+    } else {
+        Console.WriteLine("Количество чисел должно быть больше 0");
+    }
+}
---
+++ Task_43/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Задача 43
+ * Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+ */
+
+double Input(string text)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write(text);
+            return double.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Введеное не число, попробуте еще раз!");
+        }
+    }
+}
+
+
+double b1 = Input("Введите значение b1: ");
+double k1 = Input("Введите значение k1: ");
+double b2 = Input("Введите значение b2: ");
+double k2 = Input("Введите значение k2: ");
+if ((k1 == k2) && (b1 == b2))
+    Console.WriteLine("Прямые одинаковые!");
+else if (k1 == k2)
+    Console.WriteLine("Прямые параллельны!");
+else
+{
+    double x = Math.Round(-(b1 - b2) / (k1 - k2), 2);
+    double y = Math.Round(k1 * x + b1, 2);
+    Console.WriteLine($"Точка пересечения двух прямых: [x:{x}; y:{y}]");
+}
